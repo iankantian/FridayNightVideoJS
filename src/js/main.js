8e139d20971654b35bb5d107f1acc7723958f0a7
iankantian <iankantian@gmail.com>
2016-03-28 17:18:46 -0700

    standard hard-coded sample
    It’s using the videos, still needs to have the environment fixed so
    javascript methods can be used upon it.

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+$( document ).ready( function() {
+    console.log( 'document ready!' );
+    //var fridayPlayer = videojs('my-video');
+    //console.log( 'fridayPlayer looks like: ', fridayPlayer );
+});
+
+//dude!
+
+$( document ).load( function() {
+    console.log( "document loaded!" ); // doesn't fire when using videojs... interesting.
+});
